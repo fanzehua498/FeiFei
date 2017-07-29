bd689ef44c80b1182e92f06d43c5c31efbab16a8
范泽华 <fanzehua@fanzehuadeMacBook-Pro.local>
2017-07-30 03:29:02 +0800

    初始化项目

---
+++ FeiFei/FeiFei/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FeiFei
+//
+//  Created by 范泽华 on 2017/7/30.
+//  Copyright © 2017年 范泽华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FeiFei/FeiFei/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FeiFei
+//
+//  Created by 范泽华 on 2017/7/30.
+//  Copyright © 2017年 范泽华. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
